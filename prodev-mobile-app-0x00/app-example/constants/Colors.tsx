ac0ff06880aee73a9898a5ba35a3ed68d08d9826
Favour Idowu <favouridowu02@gmail.com>
2025-10-31 14:18:09 +0100

    push

---
+++ prodev-mobile-app-0x00/app-example/constants/Colors.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+
+export const colors =  {
+    primary: "#0a7ea4",
+    background: "#ffffff",
+    text: "#11181C",
+    border: "#e0e0e0",
+    card: "#f8f8f8",
+    notification: "#ff453a",
+}
